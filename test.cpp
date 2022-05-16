ca13f5df937deea3fefa2af45f447e51c0850fdf
arun <arun.kr.reddy@gmail.com>
2022-05-16 10:22:24 +0530

    20220516_1022

--- test.cpp
+++ test.cpp
@@ -1,36 +1,12 @@
-#include <assert.h>
-
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-#include <pthread.h>
+#define macro_func(a, b) \
+        printf("val " #a " : %d, " #b " : %d\n", val##a, val##b)
 
-void *thread1Func(void *ptr)
-{ //...
-}
-
-void *thread2Func(void *ptr)
-{ //...
-}
-
-int main()
+int main(void)
 {
-    pthread_t thread1;
-    pthread_t thread2;
-    int ret1, ret2;
-
-    ret1 = pthread_create(&thread1, NULL, thread1Func, NULL);
-    assert(ret1 == 0);
-    ret2 = pthread_create(&thread2, NULL, thread2Func, NULL);
-    assert(ret2 == 0);
-
-    pthread_join(thread1, NULL);
-    printf("thread1 returns: %d\n", ret1);
-
-    pthread_join(thread2, NULL);
-    printf("thread2 returns: %d\n", ret2);
-
+    int val1 = 40;
+    int val2 = 30;
+    macro_func(1, 2);
     return 0;
 }
