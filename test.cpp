fd846be4b34eab68bd3aa1f5d4e0f04165543f1e
arun <arun.kr.reddy@gmail.com>
2022-05-18 09:32:43 +0530

    20220518_0932

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 uint8_t twosComplement(uint8_t input)
 {
@@ -12,8 +13,11 @@ uint8_t twosComplement(uint8_t input)
 int main(void)
 {
     uint8_t a = 253;
+    uint8_t b = 255;
+    uint8_t *c = (uint8_t *)malloc(sizeof(uint8_t));
+    uint8_t *d = (uint8_t *)malloc(sizeof(uint8_t));
     a+=4;
-    printf("%u\n", a);
+    printf("%p %p %p %p\n", &a, &b, c, d);
 
     return 0;
 } 
