8781d7e2c48e2772308c0859f114fabc4b8284b5
Arun-Kumar REDDY (EXT) <arun-kumar.reddy.ext@valeo.com>
2022-05-23 19:52:54 +0530

    20220523_1952

--- test.cpp
+++ test.cpp
@@ -1,10 +1,68 @@
+#include <stdint.h>
 #include <stdio.h>
 
-#define MIN(A, B) ((A) <= (B) ? (A) : (B))
-
-main()
+typedef struct linked_list
 {
-    int a[2] = {10, 20};
-    int *p = a;
-    printf("%d\n", MIN(*p++, 10));
+    uint8_t value;
+    linked_list *next;
+} linked_list;
+
+void printLL(linked_list *head)
+{
+    linked_list *current = head;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->value);
+        current = current->next;
+    }
+}
+
+void deleteNode(linked_list *head, uint8_t idx)
+{
+    printf("idx %u\n", idx);
+    head = NULL;//head->next;
+    return;
+
+    if (idx == 0)
+    {
+        printf("inside");
+        head = NULL;//head->next;
+        return;
+    }
+
+    uint8_t i = 0;
+    linked_list *previous = NULL;
+    linked_list *current = head;
+    while (current != NULL)
+    {
+        previous = current;
+        current = current->next;
+        ++i;
+        if (i == idx)
+        {
+            previous->next = current->next;
+            return;
+        }
+    }
+}
+
+int main(void)
+{
+    linked_list head, middle, tail;
+
+    head.value = 10;
+    head.next = &middle;
+
+    middle.value = 20;
+    middle.next = &tail;
+
+    tail.value = 30;
+    tail.next = NULL;
+
+    deleteNode(&head, 0);
+    printf("head %u\n", head.value);
+    printLL(&head);
+
+    return 0;
 }
