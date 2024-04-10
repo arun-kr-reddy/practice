24bdae2706f8beb630737f0867f7d3f6fdf0ad75
arun <arun.kr.reddy@gmail.com>
2024-04-10 20:10:58 +0530

    202404010

--- src/test.cpp
+++ src/test.cpp
@@ -3,10 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-int
-main ()
+int main()
 {
-    printf ("hello\n");
+    printf("hello\n");
 
     return 0;
 }
+
+class animalClass  // super class
+{
+public:
+    animalClass(){};
+    ~animalClass(){};
+    void eat(){};
+    void move(){};
+    std::string name;
+    uint32_t height;
+    uint32_t weight;
+};
+
+class birdClass : public animalClass  // sub class 1
+{
+    void move() override{};  // override
+};
+
+class dogClass : public animalClass  // sub class 2
+{
+    void digHole() override{};  // extend
+};
