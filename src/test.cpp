3881a3bf956978761b0814cf6772e389d12b5fba
arun <arun.kr.reddy@gmail.com>
2024-04-26 21:16:05 +0530

    20240426

--- src/test.cpp
+++ src/test.cpp
@@ -1,33 +1,64 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// includes
+#include <algorithm>
+#include <cstdbool>
+#include <ctime>
+#include <iostream>
+#include <vector>
 
+// macros
+#define MAX_INPUT_SIZE 100
+
+// using
+using std::cout;
+using std::endl;
+using std::vector;
+
+// global
+
+// function prototypes
+uint32_t fillRandomNumbers(std::vector<uint32_t> &vec);
+uint32_t findPeak(std::vector<uint32_t> input);
+
+// functions
 int main()
 {
-    printf("hello\n");
+    vector<uint32_t> input(MAX_INPUT_SIZE);
+    fillRandomNumbers(input);
+
+    findPeak(input);
 
     return 0;
 }
 
-class animalClass  // super class
+uint32_t fillRandomNumbers(vector<uint32_t> &vec)
 {
-public:
-    animalClass(){};
-    ~animalClass(){};
-    void eat(){};
-    void move(){};
-    std::string name;
-    uint32_t height;
-    uint32_t weight;
-};
+    for (uint32_t i = 0; i < vec.size(); ++i)
+    {
+        vec.at(i) = 1;  // rand();
+    }
 
-class birdClass : public animalClass  // sub class 1
-{
-    void move() override{};  // override
-};
+    return 0;
+}
 
-class dogClass : public animalClass  // sub class 2
+uint32_t findPeak(std::vector<uint32_t> input)
 {
-    void digHole() override{};  // extend
-};
+    uint32_t half_size = input.size() / 2;
+    if (input.at(half_size - 1) > input.at(half_size))
+    {
+        std::vector<uint32_t> new_input{input.begin(),
+                                        input.begin() + half_size};
+        findPeak(new_input);
+    }
+    else if (input.at(half_size + 1) > input.at(half_size))
+    {
+        std::vector<uint32_t> new_input{input.begin() + half_size, input.end()};
+        findPeak(new_input);
+    }
+    else
+    {
+        cout << input.at(half_size - 1) << " " << input.at(half_size) << " "
+             << input.at(half_size + 1) << " " << endl;
+    }
+
+    return 0;
+}
